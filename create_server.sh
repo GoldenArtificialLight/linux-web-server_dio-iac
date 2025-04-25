444c6fe2372301d482aef80092fc0e14a37322ee
Luke <colored.vertices@gmail.com>
2025-04-25 16:13:12 -0300

    feat: Arquivo de criação do servidor web v1.0.0

---
+++ create_server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Atualizando o sistema..."
+apt-get update
+apt-get upgrade
+
+echo "Instalando o Apache..."
+apt-get install apache2 -y
+
+echo "Instalando o unzip..."
+apt-get install unzip -y
+
+echo "Baixando os arquivos de 'linux-site-dio' na pasta /tmp"
+wget -P /tmp https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactando..."
+unzip /tmp/main.zip -d /tmp
+
+echo "Copiando o website para /var/www/html"
+rm /var/www/html/index.html
+cp -R /tmp/linux-site-dio-main /var/www/html
+
+echo "Finalizado."
+echo "A página web pode ser acessada através do ip abaixo: "
+ip a
+
